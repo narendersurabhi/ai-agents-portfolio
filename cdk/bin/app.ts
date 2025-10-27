6390daa6b575967ddfdb5b54f8983bc099f1536c
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2025-10-27 04:14:35 -0400

    aws deployment

--- cdk/bin/app.ts
+++ cdk/bin/app.ts
@@ -2,5 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/pipeline-stack';
+
 const app = new cdk.App();
-new PipelineStack(app, 'AiAgentsPortfolioPipeline', { env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }});
+new PipelineStack(app, 'AiAgentsPortfolioPipeline', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
