040d06f99029166eb8975f0ce83db37507466d7a
Narender Rao Surabhi <77994312+narendersurabhi@users.noreply.github.com>
2025-10-27 14:17:58 -0400

    triggering ci/cd

--- cdk/cdk-py/app.py
+++ cdk/cdk-py/app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-import os
-import aws_cdk as cdk
+import os, aws_cdk as cdk
 from cdk_py.pipeline_stack import PipelineStack
 
+acct = os.environ.get("CDK_DEFAULT_ACCOUNT")
+region = os.environ.get("CDK_DEFAULT_REGION")
+if not acct or not region:
+    raise RuntimeError("Set CDK_DEFAULT_ACCOUNT and CDK_DEFAULT_REGION before running CDK.")
+
 app = cdk.App()
-PipelineStack(
-    app, "AiAgentsPortfolioPipeline",
-    env=cdk.Environment(
-        account=os.environ.get("CDK_DEFAULT_ACCOUNT"),
-        region=os.environ.get("CDK_DEFAULT_REGION"),
-    ),
-)
+PipelineStack(app, "AiAgentsPortfolioPipeline",
+              env=cdk.Environment(account=acct, region=region))
 app.synth()
